37a0966627d4bb1816d2a689dbff1b682ddcad33
Jason Pitts <juniorjr083@gmail.com>
2018-07-25 17:48:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,7 @@ class Student
     ;"
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute('SELECT * FROM students').flatten.last
+    @id = DB[:conn].execute('SELECT * FROM students').flatten.first
   end
   
   
