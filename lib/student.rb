30efbb235fd7affd89ba0d7c8e621c5594bf8f35
Jason Pitts <juniorjr083@gmail.com>
2018-07-25 17:32:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,11 +6,11 @@ class Student
   attr_accessor   :name, :grade
   attr_reader :id
   
-  def  initialize(name,grade)
-    @name = name 
-    @grade = grade
-    @id = nil
-     
+  def  initialize(attributes)
+    attributes.each do |k,v|
+       
+      
+    end 
   end 
   
   def self.create_table
